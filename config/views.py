b617f69900a0c0fd47e840d1d9b4152832f8ab3f
Nemes Tamás - Mechwart <nemestamas94@gmail.com>
2024-02-27 17:05:23 +0100

    small changes

--- config/views.py
+++ config/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 
+import random
+
 def index_page(request):
-    return render(request, 'index.html')
+
+    
+    return render(request, 'index.html', {'eredmeny': (21312*123) })
 
 def login_page(request):
     return render(request, 'login.html')
